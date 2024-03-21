6340e785e883c9e7c63d533e88901a85ae73947b
ScarfKid188 <grantrynders@outlook.com>
2024-03-21 18:48:28 -0400

    Added all properties to Airdrop/Category

--- ASPdemo/Entities/Airdrop.cs
+++ ASPdemo/Entities/Airdrop.cs
@@ -7,4 +7,10 @@ public class Airdrop
 {
     public int AirdropId { get; set; }
     public int CurrencyId { get; set; }
+    public string Status { get; set; }
+    public string StartDate { get; set; }
+    public string EndDate { get; set; }
+    public double TotalPrize { get; set; }
+    public double WinnerCount { get; set; }
+    public string Link { get; set; }
 }
